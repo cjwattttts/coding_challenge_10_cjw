ed02a43c0ab71c1d6671cdce88e2c0717a1ee67a
cjwattttts <cameron.jwatts@outlook.com>
2025-02-23 02:44:07 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -24,3 +24,26 @@ console.log(prod1.getDetails());
 prod1.updateStock(3);
 console.log(prod1.getDetails()); 
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+//TASK 2
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId; // sets order ID
+        this.product = product; // sets product ordered
+        this.quantity = quantity; // sets quantity
+        this.totalPrice = product.price * quantity; // calculates order price total
+        product.updateStock(quantity); // reduces stock when an order is created
+    }
+
+    getOrderDetails() { // returns order details
+        return `Order Id: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`;
+    }
+}
+
+// Test cases 
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
